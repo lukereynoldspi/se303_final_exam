62b5a9560aff150d1a368680fc2640eddf1f33f4
Luke Reynolds <lukereynoldspi@gmail.com>
2022-06-07 11:05:52 -0700

    finished split loop refactoring or cherry

--- cherry/lib/main.rb
+++ cherry/lib/main.rb
@@ -4,17 +4,28 @@
 
 def total_and_smallest
   data = [23, 15, 8, 16, 4, 42]
-  smallest = data.first
+  total = total(data)
+  smallest = smallest(data)
+  [total, smallest]
+end
+
+def total(data)
   total = 0
   data.each do |d|
     total += d
+  end
+  total
+end
+
+def smallest(data)
+  smallest = data.first
+  data.each do |d|
     if d < smallest
       smallest = d
     end
   end
-  [total, smallest]
+  smallest
 end
-
 
 # Apply the "replace loop with pipeline" refactoring.
 
