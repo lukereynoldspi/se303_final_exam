9c9513a9d1eb2372ea236d85ad802c7e96f90bf9
Luke Reynolds <lukereynoldspi@gmail.com>
2022-06-07 10:50:03 -0700

    finished apple refactoring

--- apple/lib/main.rb
+++ apple/lib/main.rb
@@ -2,13 +2,22 @@
 # To cure it, apply "extract function".
 
 def story
-  first = "Mr. Dursley was the director of a firm called Grunnings, which made drills."
-  middle = ""
-  middle << "\n"
-  middle << "\n"
-  middle << "\n"
-  middle << "\n"
-  middle << "\n"
-  conclusion = "And that was it."
-  first + middle + conclusion
+  "#{first}#{middle}#{conclusion}"
+end
+
+def first
+  "Mr. Dursley was the director of a firm called Grunnings, which made drills."
+end
+
+def middle
+  "" +
+  "\n" +
+  "\n" +
+  "\n" +
+  "\n" +
+  "\n"
+end
+
+def conclusion
+  "And that was it."
 end
